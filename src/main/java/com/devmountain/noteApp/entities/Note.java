410a840d7ba2870e7fe39b9f1b531f1bfca59160
suneetha kanumurit <suneetaraj@gmail.com>
2022-10-12 04:16:58 +0530

    first commit

---
+++ src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -0,0 +1,49 @@
+package com.devmountain.noteApp.entities;
+
+
+import com.devmountain.noteApp.dtos.NoteDto;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Notes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String body;
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    public Note(NoteDto noteDto) {
+        if(noteDto.getBody()!=null) {
+            this.body = noteDto.getBody();
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
